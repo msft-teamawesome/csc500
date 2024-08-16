2b7085fbdaa27e80355a431a596f198cdc83a778
msfttoler <toddtoler@microsoft.com>
2024-08-16 08:59:03 -0400

    Week5 Discussion Commit

---
+++ Week5/Discussion/dodgers_results_and.py
@@ -0,0 +1,7 @@
+dodgers_played_last_night = True
+dodgers_won = False
+if dodgers_played_last_night and dodgers_won:
+    print("Celebrate the Dodgers' win!")
+else:
+    print("No celebration today.")
+
---
+++ Week5/Discussion/dodgers_results_or.py
@@ -0,0 +1,6 @@
+dodgers_played_last_night = False
+dodgers_won = True
+if dodgers_played_last_night or dodgers_won:
+    print("Discuss the Dodgers' game.")
+else:
+    print("No discussion about the Dodgers today.")
